2c32d550aa8c7e3ce570a279b5548d4da10c3a61
FediGuetat <fediguetat.ytb@gmail.com>
2020-07-15 09:23:23 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import image2 from "./unnamed.jpg";
+import "./stule.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div className="d1" style={{border: 'solid 1px black'}} >  
+ 
+    	<h1 className="title red">SALEM</h1> 
+ 
+        	<br/>  
+ 
+            <img src="./Gull_portrait_ca_usa.jpg"  alt="" className="im1"/> <br /> 
+ 
+           <img src={image2} className="im2" alt="h"/>
 
+           <iframe style={{width=560 ,height=315}} src="https://www.youtube.com/embed/MLdLyzoj1Uc?start=8" frameborder="0" allow="accelerometer; autoplay; 
+           encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+ </div>  
+ )
+}
 export default App;
